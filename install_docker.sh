688704322d81c5e43cf2822774902f4a4b9c57ad
mingun <mingun0112@naver.com>
2024-12-07 12:54:29 +0900

    neo4j commit

---
+++ install_docker.sh
@@ -0,0 +1,24 @@
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo docker run \
+  --publish=7474:7474 \
+  --publish=7687:7687 \
+  --volume=$HOME/neo4j/data:/data \
+  --name=neo4j \
+  --env=NEO4J_AUTH=neo4j/password \
+  neo4j:latest
