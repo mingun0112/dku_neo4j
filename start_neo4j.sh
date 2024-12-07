f1e70ef505b8491037505b17a0bb0720c5b93193
Jeong mingyun <mingun0112@naver.com>
2024-12-07 20:51:36 +0900

    script

--- start_neo4j.sh
+++ start_neo4j.sh
@@ -1,5 +1,10 @@
 # Add Docker's official GPG key:
-sudo apt-get update
+echo "Updating system packages..."
+sudo apt update -y
+
+echo "Installing Python and pip..."
+sudo apt install python3 python3-pip -y
+
 sudo apt-get install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
@@ -15,10 +20,15 @@ sudo apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-sudo docker run \
+tmux new-session -d -s docker-demon 
+tmux send-keys -t docker-demon "dockerd" C-m 
+tmux new-session -d -s docker-neo4j    
+tmux send-keys -t docker-neo4j "docker run \
   --publish=7474:7474 \
   --publish=7687:7687 \
   --volume=$HOME/neo4j/data:/data \
   --name=neo4j \
   --env=NEO4J_AUTH=neo4j/password \
-  neo4j:latest
+  neo4j" C-m 
+
+
